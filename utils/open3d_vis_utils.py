f4447c4632abc1b6e1ef392c91a116ddaff08b90
Uzuki Ishikawajima <uzukidd@gmail.com>
2024-01-14 01:13:03 +0800

    feat: some fancy visual effects added, a bug found

--- utils/open3d_vis_utils.py
+++ utils/open3d_vis_utils.py
@@ -16,6 +16,7 @@ box_colormap = [
     [1.0, 1.0, 0],
 ]
 
+
 def get_coor_colors(obj_labels):
     """
     Args:
@@ -28,35 +29,39 @@ def get_coor_colors(obj_labels):
     max_color_num = obj_labels.max()
 
     color_list = list(colors)[:max_color_num+1]
-    colors_rgba = [matplotlib.colors.to_rgba_array(color) for color in color_list]
+    colors_rgba = [matplotlib.colors.to_rgba_array(
+        color) for color in color_list]
     label_rgba = np.array(colors_rgba)[obj_labels]
     label_rgba = label_rgba.squeeze()[:, :3]
 
     return label_rgba
 
+
 def get_transform_matrix():
-    return np.array([[0,1,0],
-                     [1,0,0],
-                     [0,0,1],])
+    return np.array([[0, 1, 0],
+                     [1, 0, 0],
+                     [0, 0, 1],])
+
 
 def draw_scenes(vis, points, gt_boxes=None, ref_boxes=None, ref_labels=None, ref_scores=None, point_colors=None, draw_origin=True, confidence=None, tracks=None):
     if isinstance(points, torch.Tensor):
-        points = points.cpu().numpy()
+        points = points.detach().cpu().numpy()
     if isinstance(gt_boxes, torch.Tensor):
-        gt_boxes = gt_boxes.cpu().numpy()
+        gt_boxes = gt_boxes.detach().cpu().numpy()
     if isinstance(ref_boxes, torch.Tensor):
-        ref_boxes = ref_boxes.cpu().numpy()
+        ref_boxes = ref_boxes.detach().cpu().numpy()
     if isinstance(tracks, torch.Tensor):
-        tracks = tracks.cpu().numpy()
-        
+        tracks = tracks.detach().cpu().numpy()
+
     # points[:,:3] = np.dot(points[:,:3] , get_transform_matrix().T)
 
-    vis.get_render_option().point_size = 1.0
+    vis.get_render_option().point_size = 3.0
     vis.get_render_option().background_color = np.zeros((3))
 
     # draw origin
     if draw_origin:
-        axis_pcd = open3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0, origin=[0, 0, 0])
+        axis_pcd = open3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=1.0, origin=[0, 0, 0])
         vis.add_geometry(axis_pcd, False)
 
     pts = open3d.geometry.PointCloud()
@@ -64,7 +69,8 @@ def draw_scenes(vis, points, gt_boxes=None, ref_boxes=None, ref_labels=None, ref
 
     vis.add_geometry(pts, False)
     if point_colors is None:
-        pts.colors = open3d.utility.Vector3dVector(np.ones((points.shape[0], 3)))
+        pts.colors = open3d.utility.Vector3dVector(
+            np.ones((points.shape[0], 3)))
     else:
         pts.colors = open3d.utility.Vector3dVector(point_colors)
 
@@ -72,32 +78,34 @@ def draw_scenes(vis, points, gt_boxes=None, ref_boxes=None, ref_labels=None, ref
         vis = draw_box(vis, gt_boxes, (0, 0, 1.0))
 
     if ref_boxes is not None:
-        vis = draw_box(vis, ref_boxes, (0, 1.0, 0), ref_labels, ref_scores, confidence)
-        
+        vis = draw_box(vis, ref_boxes, (0, 1.0, 0),
+                       ref_labels, ref_scores, confidence)
+
     if tracks is not None:
         vis = draw_tracks(vis, tracks, (0, 1.0, 0), ref_labels)
 
+
 def draw_tracks(vis, tracks, color=(0, 1.0, 0), ref_labels=None):
     for i in range(tracks.__len__()):
-        
+
         if ref_labels is None:
             uni_color = color
         else:
             uni_color = box_colormap[ref_labels[i]]
-            
+
         nodes = [node.cpu().numpy() for node in tracks[i]]
         lines = [(i, i + 1) for i in range(nodes.__len__() - 1)]
         colors = [uni_color for _ in range(nodes.__len__() - 1)]
-        
+
         line_set = open3d.geometry.LineSet()
         line_set.points = open3d.utility.Vector3dVector(nodes)
         line_set.lines = open3d.utility.Vector2iVector(lines)
         line_set.colors = open3d.utility.Vector3dVector(colors)
-        
+
         vis.add_geometry(line_set, False)
-            
-            
+
     return vis
+
 
 def translate_boxes_to_open3d_instance(gt_boxes):
     """
@@ -130,11 +138,11 @@ def translate_boxes_to_open3d_instance(gt_boxes):
 
 def draw_box(vis, gt_boxes, color=(0, 1.0, 0), ref_labels=None, score=None, confidence=None):
     for i in range(gt_boxes.shape[0]):
-        
+
         if confidence is not None:
             if score[i] < confidence[ref_labels[i]]:
                 continue
-        
+
         line_set, box3d = translate_boxes_to_open3d_instance(gt_boxes[i])
 
         if ref_labels is None:
