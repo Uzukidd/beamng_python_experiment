dfcfcac20f3a279cbcf9cda3be304e30728f1fbf
uzukidd <uzukidd@gmail.com>
2025-04-20 16:51:57 +0800

    feat: replay mode added.

--- carla_control/carla_game.py
+++ carla_control/carla_game.py
@@ -9,7 +9,9 @@ from sensor_std import lidar_carla
 
 
 class carla_client:
-    def __init__(self, host="127.0.0.1", port=2000, rendering=True, logger=None) -> None:
+    def __init__(
+        self, host="127.0.0.1", port=2000, rendering=True, logger=None
+    ) -> None:
         self.carla_client = None
         self.carla_world = None
         self.host = host
@@ -28,7 +30,9 @@ class carla_client:
         # self.carla_world = self.carla_client.load_world('Town10HD_Opt')
         self.carla_world = self.carla_client.get_world()
 
-    def debug_luanch_test_1(self,) -> None:
+    def debug_luanch_test_1(
+        self,
+    ) -> None:
         original_settings = self.carla_world.get_settings()
         settings = self.carla_world.get_settings()
         traffic_manager = self.carla_client.get_trafficmanager(8000)
@@ -50,15 +54,45 @@ class carla_client:
         vehicle_transform = vehicle_transform
         pos_idx = np.random.randint(0, vehicle_transform.__len__())
         print(
-            f"The ego vehicle has been spawned at position \t{pos_idx}/\t{vehicle_transform.__len__()}")
+            f"The ego vehicle has been spawned at position \t{pos_idx}/\t{vehicle_transform.__len__()}"
+        )
         vehicle_transform = vehicle_transform[pos_idx]
 
-        self.vehicle = self.carla_world.spawn_actor(
-            vehicle_bp, vehicle_transform)
+        self.vehicle = self.carla_world.spawn_actor(vehicle_bp, vehicle_transform)
         self.vehicle.set_autopilot(True)
 
         self.lidar_t = lidar_carla(
-            self.carla_world, self.vehicle, pcs_cache=False, need_gt=True)
+            self.carla_world, self.vehicle, pcs_cache=False, need_gt=True
+        )
+        self.lidar_t.init_lidar()
+
+    def replay_file(self, recorder_filename):
+        self.carla_client.replay_file(recorder_filename, 0.0, 0.0, 0, False)
+
+    def connect_to_vehicle(self, rolename):
+        settings = self.carla_world.get_settings()
+        traffic_manager = self.carla_client.get_trafficmanager(8000)
+        traffic_manager.set_synchronous_mode(True)
+
+        delta = 0.05
+
+        settings.fixed_delta_seconds = delta
+        settings.synchronous_mode = True
+        settings.no_rendering_mode = not self.rendering
+        self.carla_world.apply_settings(settings)
+
+        self.vehicle = None
+        while self.vehicle is None:
+            possible_vehicles = self.carla_world.get_actors().filter("vehicle.*")
+            for vehicle in possible_vehicles:
+                if vehicle.attributes["role_name"] == rolename:
+                    print("Ego vehicle found")
+                    self.vehicle = vehicle
+                    break
+
+        self.lidar_t = lidar_carla(
+            self.carla_world, self.vehicle, pcs_cache=False, need_gt=True
+        )
         self.lidar_t.init_lidar()
 
     def debug_luanch_test_2(self, rolename) -> None:
@@ -75,43 +109,57 @@ class carla_client:
 
         self.vehicle = None
         while self.vehicle is None:
-            possible_vehicles = self.carla_world.get_actors().filter('vehicle.*')
+            possible_vehicles = self.carla_world.get_actors().filter("vehicle.*")
             for vehicle in possible_vehicles:
-                if vehicle.attributes['role_name'] == rolename:
+                if vehicle.attributes["role_name"] == rolename:
                     print("Ego vehicle found")
                     self.vehicle = vehicle
                     break
-        
+
         if self.vehicle is None:
             # traffic_manager.set_synchronous_mode(False)
             import sys
+
             sys.exit(0)
 
         self.lidar_t = lidar_carla(
-            self.carla_world, self.vehicle, pcs_cache=False, need_gt=True)
+            self.carla_world, self.vehicle, pcs_cache=False, need_gt=True
+        )
         self.lidar_t.init_lidar()
 
     def generate_lidar_bp(self, arg, world, blueprint_library, delta):
         """Generates a CARLA blueprint based on the script parameters"""
         if arg["semantic"]:
-            lidar_bp = world.get_blueprint_library().find('sensor.lidar.ray_cast_semantic')
+            lidar_bp = world.get_blueprint_library().find(
+                "sensor.lidar.ray_cast_semantic"
+            )
         else:
-            lidar_bp = blueprint_library.find('sensor.lidar.ray_cast')
+            lidar_bp = blueprint_library.find("sensor.lidar.ray_cast")
             if arg["no_noise"]:
-                lidar_bp.set_attribute('dropoff_general_rate', '0.0')
-                lidar_bp.set_attribute('dropoff_intensity_limit', '1.0')
-                lidar_bp.set_attribute('dropoff_zero_intensity', '0.0')
+                lidar_bp.set_attribute("dropoff_general_rate", "0.0")
+                lidar_bp.set_attribute("dropoff_intensity_limit", "1.0")
+                lidar_bp.set_attribute("dropoff_zero_intensity", "0.0")
             else:
-                lidar_bp.set_attribute('noise_stddev', '0.2')
+                lidar_bp.set_attribute("noise_stddev", "0.2")
 
-        lidar_bp.set_attribute('upper_fov', arg["upper_fov"])
-        lidar_bp.set_attribute('lower_fov', str(arg["lower_fov"]))
-        lidar_bp.set_attribute('channels', str(arg["channels"]))
-        lidar_bp.set_attribute('range', str(arg["range"]))
-        lidar_bp.set_attribute('rotation_frequency', str(1.0 / delta))
-        lidar_bp.set_attribute('points_per_second',
-                               str(arg["points_per_second"]))
+        lidar_bp.set_attribute("upper_fov", arg["upper_fov"])
+        lidar_bp.set_attribute("lower_fov", str(arg["lower_fov"]))
+        lidar_bp.set_attribute("channels", str(arg["channels"]))
+        lidar_bp.set_attribute("range", str(arg["range"]))
+        lidar_bp.set_attribute("rotation_frequency", str(1.0 / delta))
+        lidar_bp.set_attribute("points_per_second", str(arg["points_per_second"]))
         return lidar_bp
 
     def close_client(self) -> None:
-        self.carla_client
+        self.carla_client.stop_replayer(False)
+
+        settings = self.carla_world.get_settings()
+        traffic_manager = self.carla_client.get_trafficmanager(8000)
+        traffic_manager.set_synchronous_mode(False)
+
+        settings.fixed_delta_seconds = None
+        settings.synchronous_mode = False
+        settings.no_rendering_mode = not self.rendering
+        self.carla_world.apply_settings(settings)
+
+        del self.carla_client
---
+++ carla_replay.py
@@ -0,0 +1,275 @@
+from utils import *
+import carla
+
+from detectors import torch_script_module
+from carla_control import carla_client
+
+# from utils.mods.post_processing import mono_label_distance_tracker, multi_classes_assemble_tracker
+import numpy as np
+import open3d as o3d
+import torch
+import time
+
+try:
+    import pcdet.config
+    from pcdet.datasets import DatasetTemplate
+    from pcdet.models import build_network
+    from pcdet.utils import common_utils
+except Exception as e:
+    print(e)
+    print("pcdet is not installed")
+
+import argparse
+
+
+def parse_arguments():
+    argparser = argparse.ArgumentParser(description=__doc__)
+    argparser.add_argument(
+        "--host",
+        metavar="H",
+        default="127.0.0.1",
+        help="IP of the host server (default: 127.0.0.1)",
+    )
+    argparser.add_argument(
+        "-p",
+        "--port",
+        metavar="P",
+        default=2000,
+        type=int,
+        help="TCP port to listen to (default: 2000)",
+    )
+    argparser.add_argument(
+        "-f",
+        "--recorder-filename",
+        metavar="F",
+        default="D:\\project\\scenario_runner\\manual_records\\FollowLeadingVehicleWithObstacle_1.log",
+        help="recorder filename (test1.log)",
+    )
+    argparser.add_argument(
+        "-c",
+        "--config-filename",
+        default=".\\configs\\carla_predict_eval.yaml",
+        help="recorder filename (test1.log)",
+    )
+    args = argparser.parse_args()
+    return args
+
+
+def carla_ticking(client, idx, data_dict):
+    before_time = time.perf_counter()
+
+    client.carla_world.tick()
+
+    after_time = time.perf_counter()
+    return after_time - before_time, None
+
+
+def model_forwarding(model, idx, data_dict):
+    final_boxes, final_scores, final_labels = None, None, None
+    before_time = time.perf_counter()
+
+    if data_dict["points"] is not None:
+        pred_dicts, _ = model.forward(data_dict)
+
+        if isinstance(pred_dicts, list):
+            cls_preds = pred_dicts[0]["pred_scores"]
+            box_preds = pred_dicts[0]["pred_boxes"]
+            label_preds = pred_dicts[0]["pred_labels"]
+        else:
+            cls_preds = pred_dicts["pred_scores"]
+            box_preds = pred_dicts["pred_boxes"]
+            label_preds = pred_dicts["pred_labels"]
+            selected, selected_scores = class_agnostic_nms(
+                box_scores=cls_preds, box_preds=box_preds, score_thresh=0.4
+            )
+            cls_preds = cls_preds[selected]
+            label_preds = label_preds[selected]
+            box_preds = box_preds[selected]
+
+        final_scores = cls_preds
+        final_labels = label_preds
+        final_boxes = box_preds
+    after_time = time.perf_counter()
+    return after_time - before_time, data_dict, final_boxes, final_scores, final_labels
+
+
+def scene_rendering(
+    idx,
+    points,
+    vis,
+    final_boxes=None,
+    final_scores=None,
+    final_labels=None,
+    gt_boxes=None,
+):
+    before_time = time.perf_counter()
+    if points is not None:
+        draw_scenes(
+            vis,
+            points=points[:, 1:],
+            ref_boxes=final_boxes,
+            ref_scores=final_scores,
+            ref_labels=final_labels,
+            gt_boxes=gt_boxes,
+            confidence=None,
+        )
+
+    vis.poll_events()
+    vis.update_renderer()
+    vis.clear_geometries()
+    after_time = time.perf_counter()
+    return after_time - before_time, None
+
+
+def main(args):
+    logger = create_logger()
+    client = carla_client(host=args.host, port=args.port, logger=logger)
+    client.init_client()
+    client.start_client()
+
+    cfg = pcdet.config.cfg_from_yaml_file(args.config_filename, pcdet.config.cfg)
+
+    client.replay_file(args.recorder_filename)
+    client.connect_to_vehicle("hero")
+
+    pcs_dataset = carla_point_cloud_dataset(
+        dataset_cfg=cfg.DATA_CONFIG,
+        logger=logger,
+        lidar=client.lidar_t,
+        class_names=cfg.CLASS_NAMES,
+    )
+    # object_tracker = multi_classes_assemble_tracker(num_classes=4, track_length=25, multi_head=True)
+    # try:
+    #     model = torch.jit.load("./torch_scripts/point_pillar_model.pt")
+    #     model.cuda()
+    #     model.eval()
+    # except Exception as e:
+    #     logger.error(f"Failed to load jit model from: {e}")
+    #     model = None
+
+    model = None
+
+    try:
+        # raise NotImplementedError
+        cfg_from_yaml_file("cfgs/kitti_models/pointpillar.yaml", cfg)
+        model = build_network(
+            model_cfg=cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=pcs_dataset
+        )
+        model.load_params_from_file(
+            filename="D:/modelzoo/pointpillar_7728.pth", logger=logger, to_cpu=True
+        )
+        model.cuda()
+        model.eval()
+    except Exception as e:
+        print(e)
+        logger.error("Failed to load model")
+
+    visualizers = {}
+    for channel_name in pcs_dataset.preview_channel:
+        visualizers[channel_name] = o3d.visualization.Visualizer()
+        visualizers[channel_name].create_window(
+            window_name=channel_name, width=1920, height=1080
+        )
+        # view_control = visualizers[channel_name].get_view_control()
+        # view_params = {
+        #     "boundingbox_max": np.array([69.118263244628906, 39.679920196533203, 16.415634155273438]),
+        #     "boundingbox_min": np.array([-0.059999999999999998, -39.679874420166016, -6.9146575927734375]),
+        #     "field_of_view": 60.0,
+        #     "front": np.array([-0.90307097537632919, 0.0017988087570628851, 0.42948757574567964]),
+        #     "lookat": np.array([34.529131622314452, 2.288818359375e-05, 4.75048828125]),
+        #     "up": np.array([0.42948904059539766, 0.0070563614983622357, 0.90304450154510629]),
+        #     "zoom": 0.69999999999999996
+        # }
+        # view_control.set_front(view_params["front"])
+        # view_control.set_lookat(view_params["lookat"])
+        # view_control.set_up(view_params["up"])
+        # view_control.set_zoom(view_params["zoom"])
+        # view_control.change_field_of_view(view_params["field_of_view"])
+        # view_control.set_constant_z_far(280.0)
+
+    try:
+        with torch.no_grad():
+            for idx, data_series in enumerate(pcs_dataset):
+
+                ticking_time, _ = carla_ticking(client, idx, None)
+
+                for channel_name, channel in data_series.items():
+                    data_dict = data_series[channel_name]
+                    vis = visualizers[channel_name]
+
+                    pre_time = data_dict["pre_time"]
+                    forward_time = 0.0
+                    final_boxes, final_scores, final_labels = None, None, None
+                    gt_boxes = data_dict.get("gt_boxes", None)
+
+                    if data_dict["points"] is not None:
+
+                        data_dict = pcs_dataset.collate_batch([data_dict])
+                        load_data_to_gpu(data_dict)
+
+                        if model is not None:
+                            (
+                                forward_time,
+                                data_dict,
+                                final_boxes,
+                                final_scores,
+                                final_labels,
+                            ) = model_forwarding(model, idx, data_dict)
+                        else:
+                            forward_time = 0.0
+                            final_boxes = None
+                            final_scores = None
+                            final_labels = None
+
+                        render_time, _ = scene_rendering(
+                            idx,
+                            data_dict["points"],
+                            vis,
+                            final_boxes,
+                            final_scores,
+                            final_labels,
+                            gt_boxes,
+                        )
+
+                        # render_time = time.perf_counter()
+
+                        # if final_boxes is not None:
+                        #     object_tracker.updates_object(final_boxes[np.newaxis, :, :], final_labels[np.newaxis, :], final_scores[np.newaxis, :])
+
+                        # bounding_boxes, box_scores, box_labels, tracks = object_tracker.get_all_object()
+
+                        # draw_scenes(vis,
+                        #     points=data_dict['points'][:, 1:], ref_boxes=bounding_boxes,
+                        #     ref_scores=box_scores, ref_labels=box_labels, confidence=None, tracks=tracks
+                        # )
+                        # draw_scenes(vis,
+                        #     points=data_dict['points'][:, 1:], ref_boxes=None,
+                        #     ref_scores=None, ref_labels=None, confidence=None, tracks=None
+                        # )
+                        # vis.poll_events()
+                        # vis.update_renderer()
+                        # vis.clear_geometries()
+
+                        # render_time = time.perf_counter() - render_time
+
+                        # print(f"Channel name: {channel_name}")
+                        print(
+                            f"Compute time: {pre_time:.3f} + {ticking_time:.3f} + {forward_time:.3f} + {render_time:.3f} == {pre_time + ticking_time + forward_time + render_time:.3f}s",
+                            end="\r",
+                        )
+                        # print(
+                        #     f"Target amount: {len(final_boxes if (final_boxes is not None) else [])}"
+                        # )
+                        # logger.info(f"current uuid:{object_tracker.get_last_uuid()}")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        for channel_name in pcs_dataset.preview_channel:
+            visualizers[channel_name].destroy_window()
+        client.close_client()
+        print("CarLA client closed!")
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
